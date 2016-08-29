150b433736e6eb3a0d5f47d0a069fdb74075a0ad
luke.lu <luke.lu@windfindtech.com>
2016-08-29 16:29:34 +0800

    initial

---
+++ RXNetworkReceiver.java
@@ -0,0 +1,56 @@
+package com.windfindtech.icommon.rx;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+
+import org.pmw.tinylog.Logger;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+
+/**
+ * Created by cplu on 2016/7/26.
+ */
+public class RXNetworkReceiver {
+	private static Observable<Intent> s_networkObservable;
+	private static Context m_ctx;
+
+	public static void init(Context ctx) {
+		m_ctx = ctx;
+
+		s_networkObservable = Observable.create(new Observable.OnSubscribe<Intent>() {
+			@Override
+			public void call(final Subscriber<? super Intent> subscriber) {
+				/// Note: subscriber here is a ReplaySubscriber, this call method is called only once
+				Logger.debug("subscriber in OnSubscribe :" + Thread.currentThread().getName());
+				IntentFilter intentFilter = new IntentFilter();
+				intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+				m_ctx.registerReceiver(new BroadcastReceiver() {
+					@Override
+					public void onReceive(Context context, Intent intent) {
+						String action = intent.getAction();
+						if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+							subscriber.onNext(intent);
+						}
+					}
+				}, intentFilter);
+			}
+		})
+		.replay(1)
+		.autoConnect();
+	}
+
+	public static Subscription register(RXNetworkSubscriber subscriber) {
+		return s_networkObservable.subscribe(subscriber);
+	}
+
+	public static void unregister(Subscription subscription) {
+		if (subscription != null && !subscription.isUnsubscribed()) {
+			subscription.unsubscribe();
+		}
+	}
+}
